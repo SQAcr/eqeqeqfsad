28d8127b846b2c2c8329b5ec10b74c0008e34f9d
SQAcr <57805235+SQAcr@users.noreply.github.com>
2022-02-07 19:43:41 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  token: "OTQwMjc0OTkzMzc4NTI1Mjg1.YgFBUw.3185SMRMcKUQKR_n9tHRiJKLIQc", // توكن البوت ..
+  prefix: "$", // الحرف او البداية اللي قبل الامر
+  owners: ["699041097275539456"] //ايديات اللي يقدرون يحطون بلاك لست و يشيلونه 
+}
